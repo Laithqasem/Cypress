791ddbe1de5b69333242061a1203f88636910e3e
Laith Qasem <67019009+Laithqasem@users.noreply.github.com>
2024-04-26 04:50:20 +0200

    setting up project and database

---
+++ migrations/0001_fuzzy_rockslide.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS "workspacesss" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"created_at" timestamp with time zone,
+	"workspace_owner" uuid NOT NULL,
+	"title" text NOT NULL,
+	"icon_id" text NOT NULL,
+	"data" text,
+	"in_trash" text,
+	"logo" text,
+	"banner_url" text
+);
+--> statement-breakpoint
+DROP TABLE "workspaces";
---
+++ migrations/0002_curly_carlie_cooper.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "workspacesss" RENAME TO "workspaces";
